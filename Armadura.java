de289cfe7636e20541b8be053071b82c1bf8dd0d
mahtor93 <64563975+mahtor93@users.noreply.github.com>
2022-11-02 20:06:46 -0300

    Update Armadura.java

--- Armadura.java
+++ Armadura.java
@@ -1,41 +1,46 @@
 public class Armadura {
     
     private String Nombre;
+    private int Coste;
+    private int Clase_ArmaduraDA;
+    private int Fuerza;
+    private boolean Sigilo;
+    private int Peso;
+    
     public String getNombre() {
         return Nombre;
     }
     public void setNombre(String nombre) {
         Nombre = nombre;
     }
-    private int Coste;
+
     public int getCoste() {
         return Coste;
     }
     public void setCoste(int coste) {
         Coste = coste;
     }
-    private int Clase_ArmaduraDA;
+    
     public int getClase_ArmaduraDA() {
         return Clase_ArmaduraDA;
     }
     public void setClase_ArmaduraDA(int clase_ArmaduraDA) {
         Clase_ArmaduraDA = clase_ArmaduraDA;
     }
-    private int Fuerza;
+    
     public int getFuerza() {
         return Fuerza;
     }
     public void setFuerza(int fuerza) {
         Fuerza = fuerza;
     }
-    private boolean Sigilo;
+    
     public boolean isSigilo() {
         return Sigilo;
     }
     public void setSigilo(boolean sigilo) {
         Sigilo = sigilo;
     }
-    private int Peso;
     
     public int getPeso() {
         return Peso;
