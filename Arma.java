82fd581448e3b28b399fb9afc8ef0f5e8f7e4635
mahtor93 <64563975+mahtor93@users.noreply.github.com>
2022-11-27 12:09:20 -0300

    Update Arma.java
    Simplificación de la clase arma

--- Arma.java
+++ Arma.java
@@ -3,6 +3,11 @@
 public class Arma {
 
     private String nombre;
+    private int Damage;
+    private String tipo_Arma
+    private boolean tipo_Damage;
+    
+    
     public String getNombre() {
         return nombre;
     }
@@ -11,17 +16,6 @@ public class Arma {
         this.nombre = nombre;
     }
 
-    
-    private int Coste; // hay mas de una unidad plata oro y cobre (100 platas son un oro?)
-    public int getCoste() {
-        return Coste;
-    }
-
-    public void setCoste(int coste) {
-        Coste = coste;
-    }
-
-    private int Damage;
     public int getDamage() {
         return Damage;
     }
@@ -30,7 +24,7 @@ public class Arma {
         Damage = damage;
     }
 
-    private boolean tipo_Damage;
+
     public boolean isTipo_Damage() {
         return tipo_Damage;
     }
@@ -39,36 +33,17 @@ public class Arma {
         this.tipo_Damage = tipo_Damage;
     }
 
-
-    private int Peso;
-    public int getPeso() {
-        return Peso;
+    
+    public static class tipo_damage{
+        public static String[] tipo_damage={"Contundente","Perforante","Cortante"};
     }
-
-    public void setPeso(int peso) {
-        Peso = peso;
-    }
-
 
     public static class tipo_Arma{
-        public static String[] tipo_Arma={"Armas simples cuerpo a cuerpo","Armas simples a distancia",
-        "Armas marciales cuerpo a cuerpo", "Armas marciales a distancia"};
+        public static String[] tipo_Arma={"A dos manos","Una mano","A Distancia","Ligera",
+        "Pesada", "Versatil"};
     }
 
-    public static class Propiedades {
-        public static boolean Dos_manos;
-        public static boolean Alcance;
-        public static boolean Arrojadiza;
-        public static boolean Carga;
-        public static boolean Distancia; // 
-        public static boolean Especial;
-        public static boolean Ligera;
-        public static boolean Municion;
-        public static boolean Sutil;
-        public static boolean Pesada;
-        public static boolean Versatil;
 
-    }
 
     
 }
