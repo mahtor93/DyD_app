e2d35b19947b62ee23af3fea2af1ef9f2fbd0ef9
mahtor93 <64563975+mahtor93@users.noreply.github.com>
2022-11-29 01:13:17 -0300

    Update Armadura.java

--- app/src/main/java/com/example/creacindepersonaje/Modelos/Armadura.java
+++ app/src/main/java/com/example/creacindepersonaje/Modelos/Armadura.java
@@ -1,12 +1,20 @@
-package com.example.creacindepersonaje.Modelos;
+e com.example.creacindepersonaje.Modelos;
 
 public class Armadura {
-    
+    private int idArmadura;
     private String Nombre;
     private int ClaseArmadura; //Valor base de la armadura + Destreza, armaduras pesadas no añaden destreza.
     private int Peso; //LIGERAS 8lb -> 15lb , Medias 16lb -> 40lb , Pesada 41lb -> 65lb.
     private int fuerzaReq; //Fuerza requerida para usarse. Aplicable a armaduras superiores a 40lb (Pesadas)
-    
+
+    public int getIdArmadura() {
+        return idArmadura;
+    }
+
+    public void setIdArmadura(int idArmadura) {
+        this.idArmadura = idArmadura;
+    }
+
     public String getNombre() {
         return Nombre;
     }
@@ -21,16 +29,15 @@ public class Armadura {
     public void setClaseArmadura(int claseArmadura) {
         ClaseArmadura = claseArmadura;
     }
-    
-    
+
     public int getFuerzaReq() {
         return fuerzaReq;
     }
+
     public void setFuerzaReq(int fuerzaReq) {
-        fuerzaReq = fuerzaReq;
+        this.fuerzaReq = fuerzaReq;
     }
-    
-    
+
     public int getPeso() {
         return Peso;
     }
